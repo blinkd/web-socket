f2ca8eca0b25e0c0046602c19c11c4ccb40cccb1
blinkd <1344687736@qq.com>
2018-02-07 14:03:27 +0800

    log替代print

---
+++ utils.py
@@ -0,0 +1,9 @@
+import time
+
+
+def log(*args, **kwargs):
+    # time.time() 返回 unix time
+    time_format = '%Y/%m/%d %H:%M:%S'
+    value = time.localtime(int(time.time()))
+    dt = time.strftime(time_format, value)
+    print(dt, *args, **kwargs)
